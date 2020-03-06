55293bf08090b8e56aace0427e2245f3ab4a7231
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-03-06 20:37:14 -0300

    Add Cell

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,30 @@
-object Main extends App {
-  println("Main")
+import cats.Show
+
+import cats.effect.{ IO, IOApp, ExitCode }
+
+object Main extends IOApp {
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val c = new Cell[IO]
+    println(c.show)
+    IO(ExitCode.Success)
+  }
+}
+
+case class Cell[F[_]]() {
+  // import cats.effect.concurrent.Semaphore
+
+  // val used: Semaphore[F] = ???
+  val value: Option[Char] = None
+
+  implicit val showCell: Show[Cell[F]] =
+    Show.show { cell =>
+      val i: Char = cell.value.getOrElse(' ')
+      s"""+---+
+          || $i |
+          |+---+""".stripMargin
+    }
+
+  def show(): String =
+    Show[Cell[F]].show(this)
 }
