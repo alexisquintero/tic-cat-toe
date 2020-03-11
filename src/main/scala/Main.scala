8b58caf361c3ec58b2b1e20476e5c8848b480987
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-03-10 21:35:15 -0300

    Add board and moves

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,50 @@
 import cats.Show
 
-import cats.effect.{ IO, IOApp, ExitCode }
+import cats.effect.{ IO, IOApp, ExitCode, Sync }
+import cats.effect.concurrent.Ref
 
 object Main extends IOApp {
 
-  def run(args: List[String]): IO[ExitCode] = {
-    val c = new Cell[IO]
-    println(c.show)
-    IO(ExitCode.Success)
-  }
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      board <- Ref.of[IO, Vector[Vector[Char]]](Vector[Vector[Char]](
+                 Vector('q', 'w', 'e'),
+                 Vector('a', 'b', 'd'),
+                 Vector('z', 'x', 'c')))
+      _ <- Game.showBoard[IO](board)
+      x <- Game.humanAction[IO]
+      y <- Game.cpuAction[IO]
+    } yield (ExitCode.Success)
+}
+
+case object Game {
+  import scala.io.StdIn
+
+  import cats.syntax.flatMap._
+  import cats.syntax.functor._
+
+  def showBoard[F[_]: Sync](board: Ref[F, Vector[Vector[Char]]]): F[Unit] =
+    for {
+      pieces <- board.get
+      _ <- Sync[F].delay(println(pieces))
+    } yield ()
+
+  def humanAction[F[_]: Sync](): F[Unit] =
+      for {
+        _ <- Sync[F].delay(println("Your move: "))
+        ans <- Sync[F].delay(StdIn.readLine)
+        _ <- Sync[F].delay(println(ans))
+      } yield ()
+
+  def cpuAction[F[_]: Sync](): F[Unit] =
+    for {
+      _ <- Sync[F].delay(println("My move: "))
+      _ <- Sync[F].delay(println("move"))
+    } yield ()
 }
 
 case class Cell[F[_]]() {
-  // import cats.effect.concurrent.Semaphore
 
-  // val used: Semaphore[F] = ???
   val value: Option[Char] = None
 
   implicit val showCell: Show[Cell[F]] =
