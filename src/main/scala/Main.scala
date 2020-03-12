49138259b13f480070a6dc7e2e5a67323e4b3053
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-03-12 17:01:25 -0300

    Add human move

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,18 +10,36 @@ object Main extends IOApp {
     for {
       initialValues <- IO(List('q', 'w', 'e', 'a', 'b', 'd', 'z', 'x', 'c'))
       board <- IO(Board(initialValues))
+      _     <- IO(println(s"Posible positions: ${Position.showAllPositions}"))
       game  <- Game.newGame[IO, Char](board)
-      _     <- Game.showBoard[IO, Char](board)
-      // x     <- Game.humanAction[IO]
-      // y     <- Game.cpuAction[IO]
+      _     <- Game.gameLoop[IO, Char](board, 'x')
     } yield (ExitCode.Success)
 }
 
 trait Position
 
 object Position {
+  import cats.syntax.option._
+
   val allPositions: List[Position] =
     List(TopLeft, TopMiddle, TopRight, MiddleLeft, MiddleMiddle, MiddleRight, BottomLeft, BottomMiddle, BottomRight)
+
+  def showAllPositions(): String =
+    allPositions.map(_.toString).reduce((acc, cur) => s"$acc, $cur")
+
+  def fromString(s: String): Option[Position] =
+    s.toLowerCase match {
+      case "topleft"      => TopLeft.some
+      case "topmiddle"    => TopMiddle.some
+      case "topright"     => TopRight.some
+      case "middleleft"   => MiddleLeft.some
+      case "middlemiddle" => MiddleMiddle.some
+      case "middleright"  => MiddleRight.some
+      case "bottomleft"   => BottomLeft.some
+      case "bottommiddle" => BottomMiddle.some
+      case "bottomright"  => BottomRight.some
+      case _              => none
+    }
 }
 
 case object TopLeft      extends Position
@@ -58,9 +76,12 @@ case class Board[A: Show](positions: Map[Position, A]) {
     def show(): String =
       Show[Board[A]](showBoard).show(this)
 
+    def move(position: Position, symbol: A): Board[A] =
+      this.copy(positions = this.positions + (position -> symbol))
 }
 
 case object Board {
+  //TODO: Better checking initial values when different than 9 elements
   def apply[A: Show](initialValues: List[A]): Board[A] = {
     val m: Map[Position, A] = (Position.allPositions zip initialValues).toMap
     Board(m)
@@ -79,16 +100,31 @@ case object Game {
   def showBoard[F[_]: Sync, A](board: Board[A]): F[Unit] =
     Sync[F].delay(println(board.show))
 
-  def humanAction[F[_]: Sync](): F[Unit] =
+  def humanAction[F[_]: Sync, A](board: Board[A], playerA: A): F[Board[A]] =
       for {
-        _ <- Sync[F].delay(println("Your move: "))
-        ans <- Sync[F].delay(StdIn.readLine)
-        _ <- Sync[F].delay(println(ans))
-      } yield ()
+        _        <- Sync[F].delay(println("Your move: "))
+        ans      <- Sync[F].delay(StdIn.readLine)
+        posObj   =  Position.fromString(ans)
+        newBoard <- posObj match {
+                      case None => for {
+                        _ <- Sync[F].delay(println("Invalid position"))
+                        _ <- humanAction(board, playerA)
+                      } yield board
+                      case Some(position) => Sync[F].delay(board.move(position, playerA))
+                    }
+      } yield newBoard
 
   def cpuAction[F[_]: Sync](): F[Unit] =
     for {
       _ <- Sync[F].delay(println("My move: "))
       _ <- Sync[F].delay(println("move"))
+    } yield ()
+
+  def gameLoop[F[_]: Sync, A](board: Board[A], playerA: A): F[Unit] =
+    for {
+      _          <- Game.showBoard[F, A](board)
+      humanBoard <- Game.humanAction[F, A](board, playerA)
+      // y <- Game.cpuAction[F]
+      _          <- gameLoop(humanBoard, playerA)
     } yield ()
 }
