c76755669dfc3a621bdfa0ce5b82a635c857ce84
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-03-16 11:32:21 -0300

    Add Position tests

--- build.sbt
+++ build.sbt
@@ -3,6 +3,9 @@ name := "tic-cat-toe"
 scalaVersion := "2.13.1"
 
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.1.1" withSources() withJavadoc()
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.1" % "test"
+libraryDependencies += "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % "test"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.3" % "test"
 
 scalacOptions ++= Seq(
   "-encoding", "UTF-8",
---
+++ src/test/scala/PositionsSpec.scala
@@ -0,0 +1,34 @@
+import org.scalacheck.Prop.{ forAll, propBoolean }
+import org.scalacheck.Gen
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.Checkers
+
+import cats.syntax.option._
+
+import Positions._
+
+case class PositionsSpec() extends AnyFlatSpec with Checkers with Matchers {
+  val positionGen =
+    Gen.oneOf(Position.allPositions)
+
+  "fromString" should "parse valid Positions" in {
+    check {
+      forAll(positionGen) { position =>
+        Position.fromString(position.toString.toLowerCase) == position.some
+      }
+    }
+  }
+
+  "fromString" should "return none with invalid Positions" in {
+    check {
+      val validPositions: List[String] = Position.allPositions.map(_.toString)
+
+      forAll { position: String =>
+        (! validPositions.exists(_ == position)) ==>
+          (Position.fromString(position.toString.toLowerCase) == none)
+      }
+    }
+  }
+}
