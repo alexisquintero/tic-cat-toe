50e6fdff5cd0d385e0587a7fd012b6d63defcaa3
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2020-03-17 19:14:32 -0300

    Parameterize empty board gen

--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -5,7 +5,9 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.Checkers
 
-import cats.effect.IO
+import cats.{ Show, Eq }
+
+import cats.effect.{ IO, Sync }
 
 import Board._
 import Domain.Symbols
@@ -14,66 +16,48 @@ import Positions._
 
 case class BoardSpec() extends AnyFlatSpec with Checkers with Matchers {
 
+  def emptyBoardGen[A: Show: Eq](gen: Gen[A]): Gen[Board[A]] =
+    for {
+      human <- gen
+      cpu   <- gen
+      empty <- gen
+      players = Map[Player, A](Human -> human, Cpu -> cpu)
+      symbols = Symbols(players, empty)
+      if (human != cpu) && (human != empty) && (cpu != empty)
+    } yield Board[A](symbols)
+
   import cats.instances.char.catsStdShowForChar
   import cats.instances.char.catsKernelStdOrderForChar
 
-  val emptyBoardCharGen =
-    for {
-      human <- Gen.alphaChar
-      cpu   <- Gen.alphaChar
-      empty <- Gen.alphaChar
-      players = Map[Player, Char](Human -> human, Cpu -> cpu)
-      symbols = Symbols(players, empty)
-      if (human != cpu) && (human != empty) && (cpu != empty)
-    } yield Board[Char](symbols)
-
-  "validPositions with char" should "return all valid positions for this board" in {
-    check {
-      forAll(emptyBoardCharGen) { board =>
-        board.validPositions[IO].unsafeRunSync.size == Position.allPositions.size
-      }
-    }
-  }
+  val emptyBoardCharGen: Gen[Board[Char]] =
+    emptyBoardGen[Char](Gen.alphaChar)
 
   import cats.instances.string.catsStdShowForString
   import cats.instances.string.catsKernelStdOrderForString
 
-  val emptyBoardStringGen =
-    for {
-      human <- Gen.alphaStr
-      cpu   <- Gen.alphaStr
-      empty <- Gen.alphaStr
-      players = Map[Player, String](Human -> human, Cpu -> cpu)
-      symbols = Symbols(players, empty)
-      if (human != cpu) && (human != empty) && (cpu != empty)
-    } yield Board[String](symbols)
-
-  "validPositions with string" should "return all valid positions for this board" in {
-    check {
-      forAll(emptyBoardStringGen) { board =>
-        board.validPositions[IO].unsafeRunSync.size == Position.allPositions.size
-      }
-    }
-  }
+  val emptyBoardStringGen: Gen[Board[String]] =
+    emptyBoardGen[String](Gen.alphaNumStr)
 
   import cats.instances.int.catsStdShowForInt
   import cats.instances.int.catsKernelStdOrderForInt
 
-  val emptyBoardPosIntGen =
-    for {
-      human <- Gen.posNum[Int]
-      cpu   <- Gen.posNum[Int]
-      empty <- Gen.posNum[Int]
-      players = Map[Player, Int](Human -> human, Cpu -> cpu)
-      symbols = Symbols(players, empty)
-      if (human != cpu) && (human != empty) && (cpu != empty)
-    } yield Board[Int](symbols)
+  val emptyBoardPosIntGen: Gen[Board[Int]] =
+    emptyBoardGen[Int](Gen.posNum[Int])
 
-  "validPositions with positive integers" should "return all valid positions for this board" in {
-    check {
-      forAll(emptyBoardPosIntGen) { board =>
-        board.validPositions[IO].unsafeRunSync.size == Position.allPositions.size
+  def oneMoveBoardCharGen[F[_]: Sync, A](gen: Gen[Board[A]]) =
+    gen.flatMap(board => board.move[F](TopLeft, Human))
+
+  def validPositionsSpec[A](genType: String, gen: Gen[Board[A]]): Unit = {
+    s"validPositions with $genType" should "return all valid positions" in {
+      check {
+        forAll(gen) { board =>
+          board.validPositions[IO].unsafeRunSync.size == Position.allPositions.size
+        }
       }
     }
   }
+
+  validPositionsSpec("Char", emptyBoardCharGen)
+  validPositionsSpec("String", emptyBoardStringGen)
+  validPositionsSpec("Positive Int", emptyBoardPosIntGen)
 }
